6675167d6ebf8681874d29e51821a54ef6cf63bb
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-01-26 20:44:19 +0900

    6.7 정리

--- src/main/java/com/ksb/spring/UserService.java
+++ src/main/java/com/ksb/spring/UserService.java
@@ -1,12 +1,19 @@
 package com.ksb.spring;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
-    User get(String id);
-    List<User> getAll();
     void deleteAll();
     void update(User user1);
     void upgradeLevels();
+
+    @Transactional(readOnly = true)
+    User get(String id);
+
+    @Transactional(readOnly = true)
+    List<User> getAll();
 }
